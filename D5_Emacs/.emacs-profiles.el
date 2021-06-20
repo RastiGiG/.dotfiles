03de7fa2f664cce901903493e107779a8e6a3008
RastiGiG <randomly.ventilates@simplelogin.co>
2021-06-20 22:53:10 +0200

    Reordered .dotfiles directory. Added OrgFiles-folder and more

---
+++ D5_Emacs/.emacs-profiles.el
@@ -0,0 +1,7 @@
+(("default" . ((user-emacs-directory . "~/.dotfiles/C1_EmacsConfiguration/.emacs.d")))
+
+ ("spacemacs" . ((user-emacs-directory . "~/.config/emacs-configs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.dotfiles/C1_EmacsConfiguration/.spacemacs.d")))))
+
+ ("doom" . ((user-emacs-directory . "~/.config/emacs-configs/doom-emacs")
+                 (env . (("DOOMDIR" . "~/.dotfiles/C1_EmacsConfiguration/.doom.d"))))))
--- abbrev_defs
+++
@@ -1,6 +0,0 @@
-;;-*-coding: utf-8;-*-
-(define-abbrev-table 'global-abbrev-table
-  '(
-    ("elp" "emacs-lisp" nil :count 2)
-   ))
-
