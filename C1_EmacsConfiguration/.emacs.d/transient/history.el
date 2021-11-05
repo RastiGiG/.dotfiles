73f3e03ca6c1a64798e7418d1fc6ddaf29a4008d
RastiGiG <randomly.ventilates@simplelogin.co>
2021-11-05 23:07:47 +0100

    Emacs: Updated History and Org Roam DB Files

--- C1_EmacsConfiguration/.emacs.d/transient/history.el
+++ C1_EmacsConfiguration/.emacs.d/transient/history.el
@@ -1,11 +1,15 @@
-((magit-cherry-pick
+((magit-branch nil)
+ (magit-cherry-pick
   ("--ff"))
  (magit-commit nil)
  (magit-diff
   ("--no-ext-diff" "--stat"))
  (magit-dispatch nil)
+ (magit-fetch nil)
  (magit-log
   ("-n256" "--graph" "--decorate"))
+ (magit-merge nil)
+ (magit-pull nil)
  (magit-push nil)
  (magit-rebase
   ("--autostash")))
