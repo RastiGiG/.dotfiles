1ee5dda42f382573a3d6be8fd0e3de86413947ae
RastiGiG <randomly.ventilates@simplelogin.co>
2022-07-17 16:59:42 +0200

    Emacs: Added Config for Password Store

--- C01_EmacsConfiguration/.emacs.d/init.el
+++ C01_EmacsConfiguration/.emacs.d/init.el
@@ -1267,6 +1267,23 @@ _h_: ?mode   | _C--_: show less   | _*_: *thing  | _q_: quit hdrs | _j_: jump2ma
     )
   )
 
+;; Add wrapper for the command line tool 'pass'
+(use-package password-store
+      :config
+      ;; If you want to adjust the default password length
+      ;; (setq password-store-password-length 12)
+
+      ;; Use Password Store as Source for Auth-Sources
+      (setq auth-sources '(password-store))
+      )
+
+;; Add Functions to Leader Keys
+(pet/leader-keys
+      "ap"  '(:ignore t :which-key "Password Store")
+      "app" 'password-store-copy
+      "api" 'password-store-insert
+      "apg" 'password-store-generate)
+
 ;; Use EBDB for contact management
 (use-package ebdb
       :config
