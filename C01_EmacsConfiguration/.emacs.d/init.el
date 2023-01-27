569f8b36e63ade74689c8c0f7b0680f208f912df
RastiGiG <randomly.ventilates@simplelogin.co>
2023-01-27 18:07:51 +0100

    Emacs: Added Citar for Citation Handling

--- C01_EmacsConfiguration/.emacs.d/init.el
+++ C01_EmacsConfiguration/.emacs.d/init.el
@@ -855,6 +855,44 @@
 
       )
 
+;; Add Citar completion buffer for citations
+      (use-package citar
+	;; The `:straight' keyword is not necessary. However, I do this to set a value
+	;; for the `:includes' keyword. This keyword tells use-package that those
+	;; package(s) are provided by this package, and not to search for them on
+	;; Melpa for download. Alternatively, you can set the `:straight' keyword to
+	;; nil in those package(s) use-package declaration.
+	:straight (citar :type git :host github :repo "emacs-citar/citar" :includes (citar-org))
+	:custom
+	;; A list of bib files. A good idea would be having its value be identical to
+	;; that of `org-cite-global-bibliography'. For me, I have all my bib file(s)
+	;; as a list of strings in `kb/bib-files'.
+	(citar-bibliography (concat pet/bibliographies "/Main_Bib.bib"))
+
+	;; List of directories for reference nodes
+	(citar-notes-paths (list pet/org-dir))
+
+	(citar-open-note-function 'orb-citar-edit-note) ; Open notes in `org-roam'
+	;; (citar-at-point-function 'embark-act)           ; If use `embark'
+
+	;; Add Keybindings
+	:general
+	(:keymaps 'org-mode-map
+			      :prefix "C-c b"
+			      "b" '(citar-insert-citation :wk "Insert citation")
+			      "r" '(citar-insert-reference :wk "Insert reference")
+			      "o" '(citar-open-notes :wk "Open note"))
+	)
+
+;; Use `citar' with `org-cite'
+(use-package citar-org
+      :after oc
+      :custom
+      (org-cite-insert-processor 'citar)
+      (org-cite-follow-processor 'citar)
+      (org-cite-activate-processor 'citar)
+      )
+
 ;; Use Helpful to get a better help buffer
 (use-package helpful
   :custom
