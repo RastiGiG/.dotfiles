ac9e756eed14e29e6e223f1077d5d27789796a5b
RastiGiG <randomly.ventilates@simplelogin.co>
2023-12-16 16:43:40 +0100

    Emacs: Disabled EBDB for now

--- C01_EmacsConfiguration/.emacs.d/init.el
+++ C01_EmacsConfiguration/.emacs.d/init.el
@@ -325,7 +325,7 @@ Note: it depends on s.el."
 		shell-mode-hook
 		eshell-mode-hook
 		pdf-view-mode-hook
-		treemacs-mode))
+		treemacs-mode-hook))
       (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Set default font face of present
@@ -1544,7 +1544,7 @@ _h_: ?mode   | _C--_: show less   | _*_: *thing  | _q_: quit hdrs | _j_: jump2ma
       ;; Install PDF Tools in all buffers
       (pdf-tools-install)
       ;; Disable linum mode in PDF Tools
-      (add-hook 'pdf-view-mode-hook (lambda () (linum-mode -1)))
+      ;; (add-hook 'pdf-view-mode-hook (lambda () (linum-mode -1)))
 
       (pet/leader-keys
 	;; Toggles - Modes
@@ -1567,35 +1567,6 @@ _h_: ?mode   | _C--_: show less   | _*_: *thing  | _q_: quit hdrs | _j_: jump2ma
       "app" 'password-store-copy
       "api" 'password-store-insert
       "apg" 'password-store-generate)
-
-;; Use EBDB for contact management
-(use-package ebdb
-      :config
-      ;; Set the source files for Contact DBs
-      (setq ebdb-sources (list                        
-					      (concat pet/home-dir "Contacts/default-contacts.db")
-					      (concat pet/home-dir "Contacts/family.db")
-					      (concat pet/home-dir "Contacts/work.db")
-					      (concat pet/home-dir "Contacts/organizations.db")
-					      (concat pet/home-dir "Contacts/mailing-lists.db")
-					      ))
-
-      ;; Access Menu through '.' in EBDB Buffer
-      ;; (define-key ebdb-mode-map
-      ;;		  "." 'hydra-ebdb-menu/body)		  
-
-      ;; Specify the Display Format for Month and Day on Anniversaries
-      ;; (setq ebdb-anniversary-md-format "%B %d")
-      ;; Specify the Display Format for Year, Month and Day on Anniversaries
-      ;; (setq ebdb-anniversary-ymd-format "%B %d, %Y")
-
-      ;; Set Keybindings
-      (pet/leader-keys
-	"c"  '(:ignore t :which-key "Contacts")
-	"co" '(ebdb-open :which-key "Open Contact Database")
-
-	)
-      )
 
 ;; Store Location of Account Settings
 (setq pet/mail-accounts-config
