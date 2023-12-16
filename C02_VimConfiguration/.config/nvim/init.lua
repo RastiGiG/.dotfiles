edc8198028295e4acb975b856e8d97cddd2726a8
RastiGiG <randomly.ventilates@simplelogin.co>
2023-12-16 16:41:00 +0100

    NVIM: Setup NeoVim Config (converted from Kickstart-NVim)

---
+++ C02_VimConfiguration/.config/nvim/init.lua
@@ -0,0 +1,34 @@
+--[[
+
+  _   _         __     ___           
+ | \ | | ___  __\ \   / (_)_ __ ___  
+ |  \| |/ _ \/ _ \ \ / /| | '_ ` _ \ 
+ | |\  |  __/ (_) \ V / | | | | | | |
+ |_| \_|\___|\___/ \_/  |_|_| |_| |_|
+
+
+ - NeoVim - the new V(IM)proved
+ Source:         - .dotfiles/00_OrgFiles/NeoVimConfig.org
+ Target:         - .dotfiles/C02_VimConfiguration/.config/nvim/init.lua
+ Author Email:   - randomly.ventilates@simplelogin.co
+ Author GitHub:  - https://github.com/RastiGiG/
+
+--]]
+
+--[[
+
+=====================================================================
+==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
+
+  If you don't know anything about Lua, I recommend taking some time to read through
+  a guide. One possible example:
+  - https://learnxinyminutes.com/docs/lua/
+
+  And then you can explore or search through `:help lua-guide`
+  - https://neovim.io/doc/user/lua-guide.html
+
+--]]
+
+-- Load personal configuration
+require('gigus')
