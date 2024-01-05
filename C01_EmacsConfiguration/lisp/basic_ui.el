11f8de95d5baa2ccf8a4012ec0b5079004abbf8d
RastiGiG <randomly.ventilates@simplelogin.co>
2024-01-05 13:10:42 +0100

    Emacs: Updated Basic UI Config (Dired and Tab Bar Settings)

--- C01_EmacsConfiguration/lisp/basic_ui.el
+++ C01_EmacsConfiguration/lisp/basic_ui.el
@@ -5,16 +5,14 @@
 ;; |____/_/   \_\____/___\____|  \___/|___|  \____\___/|_| \_|_|   |___\____|
 ;;
 
-;; A few basic settings
-
-(setq inhibit-startup-message t)
-
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
-
 (menu-bar-mode -1)          ; Disable the menu bar
+
+;; We don't need to see that
+(setq inhibit-startup-message t)
 
 ;; Start Emacs in Fullscreen mode and set transparancy
 (add-hook 'emacs-startup-hook 'toggle-frame-maximized)
@@ -23,13 +21,6 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(alpha . (95 . 95)))
 
-;; Set default Encoding to UTF-8
-(set-language-environment "UTF-8")
-(set-default-coding-systems 'utf-8)
-
-;; Set up the visible bell
-(setq visible-bell t)
-
 ;; Set Column Numbers
 (column-number-mode)
 ;; Set Line Numbers Globally
@@ -37,26 +28,39 @@
 
 ;; Enable line numbers for some modes
 (dolist (mode '(text-mode-hook
-				prog-mode-hook
-				conf-mode-hook))
-      (add-hook mode (lambda () (display-line-numbers-mode 'relative))))
+  			prog-mode-hook
+  			conf-mode-hook))
+(add-hook mode (lambda () (display-line-numbers-mode 'relative))))
 
-;; Set Visual Line Mode for text modes only
-;; Preferred over global-visual-line-mode
-;; (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
+;; Disable line numbers for some modes
+(dolist (mode
+         '(;;org-mode-hook
+  	     term-mode-hook
+  	     vterm-mode-hook
+  	     shell-mode-hook
+  	     eshell-mode-hook
+  	     pdf-view-mode-hook
+  	     treemacs-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Set up the visible bell
+(setq visible-bell t)
 
 ;; Enable Highlight-Line
 (hl-line-mode 1)
 
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		vterm-mode-hook
-		shell-mode-hook
-		eshell-mode-hook
-		pdf-view-mode-hook
-		treemacs-mode-hook))
-      (add-hook mode (lambda () (display-line-numbers-mode 0))))
+;; Add a little space when displaying buffers
+(setq-default line-spacing 0.12)
+
+;; Set default Encoding to UTF-8
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+;; Display battery for when in full screen mode
+(display-battery-mode t)
+
+;; Don't show windowed Dialog Box on Prompts
+(setq use-dialog-box nil)
 
 ;; Set default font face of present
 (when (pet/font-available-p "Iosevka")
@@ -87,3 +91,42 @@
 (set-fontset-font t '(#xF500 . #xF8FF) "Symbols Nerd Font")
 (set-fontset-font t 'unicode "Symbola" nil 'append)
 (set-fontset-font t 'unicode (font-spec :script 'unicode) nil 'append)
+
+;; Configuring Dired
+
+;; The prefixes are arguments given to "ls" by dired
+(setq dired-listing-switches
+       "-aghlv --group-directories-first")
+
+;; Set dired jump keymap
+(global-set-key (kbd "C-x C-j") 'dired-jump)
+
+;; Tab Bar Mode Setting
+
+;; Set new tab to scratch buffer
+(setq tab-bar-new-tab-choice "*scratch*")
+;; right is default -
+;; change if you dont like that
+;; (tab-bar-new-tab-to right)                
+
+;; Set the name of the tab to
+;; match the current buffer
+;; (setq tab-bar-tab-name-function
+;;       tab-bar-current-tab-name)
+
+      ;; Keyboard Rules
+      ;; Remove Tab Bar Buttons
+      (setq tab-bar-close-button-show nil
+		tab-bar-new-button-show nil
+		;; tab-bar-button-relief               ;; controls outline of buttons
+		;; tab-bar-face tab-bar-tab            ;; configure tab face (bgcolor etc.)
+		)
+
+      ;; tab bar is not automatically shown
+      ;; (set 1 to enable)
+      (setq tab-bar-show nil)                      
+
+      ;; Helper function to get only the name
+      ;; of current tab
+      (defun pet/current-tab-name ()
+	(alist-get 'name (tab-bar--current-tab)))
