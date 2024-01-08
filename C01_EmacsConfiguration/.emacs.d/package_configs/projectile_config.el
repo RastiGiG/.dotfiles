cc51fd2cc0ccc85a78de4aca3b854a10993adae5
RastiGiG <randomly.ventilates@simplelogin.co>
2024-01-08 10:37:06 +0100

    Emacs: Set exluded Directories and Files for Grep with Projectile

--- C01_EmacsConfiguration/.emacs.d/package_configs/projectile_config.el
+++ C01_EmacsConfiguration/.emacs.d/package_configs/projectile_config.el
@@ -1,21 +1,35 @@
 ;; Use Projectile for project management
 (use-package projectile
   :diminish projectile-mode
-  :config (projectile-mode)
-  ;; Setup default keybinding for projectile
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
+  :demand t
   :init
   ;; NOTE: Set this to the folder where you keep your Git repos!
   (when (file-directory-p "~/Projects/Programming")
     (setq projectile-project-search-path
-	  '("~/Projects/Programming")))
+          '("~/Projects/Programming")))
   (setq projectile-switch-project-action
-	#'projectile-dired)
-      ;; Add Projectile Functions to User Leader Keys
+        #'projectile-dired)
+
+  ;; Activate Projectile
+  (projectile-mode)
+  ;; Setup default keybinding for projectile
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :config
+  ;; Specify a list of directories and files to ignore by projectile grep
+  ;; Can interpret regular expressions
+  (cl-loop for file in
+           '()
+           do
+           (add-to-list 'projectile-globally-ignored-files file))
+  (cl-loop for directory in
+           '("backup")
+           do
+           (add-to-list 'projectile-globally-ignored-directories directory))
+
+  ;; Add Projectile Functions to User Leader Keys
   (pet/leader-keys
-   "p"  '(:ignore t :which-key "projects")
-   ;; "pF"  'consult-ripgrep
-   "pc"  'projectile-compile-project
-   "pd"  'projectile-dired)
-  )
+    "p"  '(:ignore t :which-key "projects")
+    ;; "pF"  'consult-ripgrep
+    "pc"  'projectile-compile-project
+    "pd"  'projectile-dired))
