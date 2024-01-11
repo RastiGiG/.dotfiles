dee955981bfb5698c8f6eafcd43aaf74773b5f9d
RastiGiG <randomly.ventilates@simplelogin.co>
2024-01-11 17:05:58 +0100

    Emacs: Updated Doom Themes Config (added visual bell)

--- C01_EmacsConfiguration/.emacs.d/package_configs/themes_config.el
+++ C01_EmacsConfiguration/.emacs.d/package_configs/themes_config.el
@@ -1,10 +1,11 @@
 ;; Load Doom Themes
 (use-package doom-themes
-      :init (load-theme 'doom-dracula t)
-      :config
-      (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-  		doom-themes-enable-italic t) ; if nil, italics is universally disabled
-      ;; Enable custom neotree theme (all-the-icons must be installed!)
-      (doom-themes-neotree-config)
-      ;; Corrects (and improves) org-mode's native fontification.
-      (doom-themes-org-config))
+    :init (load-theme 'doom-dracula t)
+    :config
+    (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+    	      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+    ;; Enable custom neotree theme (all-the-icons must be installed!)
+    (doom-themes-neotree-config)
+    ;; Corrects (and improves) org-mode's native fontification.
+    (doom-themes-org-config)
+    (doom-themes-visual-bell-config))
