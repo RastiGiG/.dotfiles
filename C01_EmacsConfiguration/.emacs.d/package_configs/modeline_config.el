df3aa5b7ec99c199095a8897af1a9a6b8b945a95
RastiGiG <randomly.ventilates@simplelogin.co>
2024-01-13 18:47:12 +0100

    Emacs: Updated Doom Modeline Config (added clock etc.)

--- C01_EmacsConfiguration/.emacs.d/package_configs/modeline_config.el
+++ C01_EmacsConfiguration/.emacs.d/package_configs/modeline_config.el
@@ -4,7 +4,17 @@
   ;; Activate Doom Modeline
   :init (doom-modeline-mode 1)
   :config
-  (setq doom-modeline-height 35      ;; sets modeline height
-        doom-modeline-bar-width 5    ;; sets right bar width
-        doom-modeline-persp-name t   ;; adds perspective name to modeline
-        doom-modeline-persp-icon t)) ;; adds folder icon next to persp name
+  (setq doom-modeline-height 35                   ;; sets modeline height
+        doom-modeline-bar-width 5                 ;; sets right bar width
+        doom-modeline-persp-name t                ;; adds perspective name to modeline
+        doom-modeline-window-width-limit nil
+        doom-modeline-buffer-file-name-style 'truncate-with-project
+        doom-modeline-minor-modes nil             ;; don’t try to display minor mode names
+        doom-modeline-enable-word-count t         ;; display word count
+        doom-modeline-buffer-encoding t           ;; show encoding for current buffer
+        doom-modeline-buffer-modification-icon t  ;; indicate modification
+        doom-modeline-env-python-executable "python"
+        ;; needs display-time-mode to be one
+        doom-modeline-time t
+        doom-modeline-vcs-max-length 12           ;; length of VCS branch name displayed - default 12
+        doom-modeline-persp-icon t))              ;; adds folder icon next to persp name
