6bd99839eeea411ccc2139d6357983ffbe7755a8
RastiGiG <randomly.ventilates@simplelogin.co>
2024-01-13 21:56:14 +0100

    Emacs: Updated Dashboard to include custom finances function

--- C01_EmacsConfiguration/.emacs.d/package_configs/dashboard_config.el
+++ C01_EmacsConfiguration/.emacs.d/package_configs/dashboard_config.el
@@ -1,3 +1,39 @@
+;; Function to insert vocabulary
+(defun pet/dashboard-insert-vocabulary (list-size)
+  (dashboard-insert-heading "Word of the Day:"
+  						      nil
+  						      (all-the-icons-faicon "newspaper-o"
+  												:height 1.2
+  												:v-adjust 0.0
+  												:face 'dashboard-heading))
+  (insert "\n")
+  (let ((random-line nil)
+  	      (lines nil))
+      (with-temp-buffer
+  	(insert-file-contents (concat user-emacs-directory "words"))
+  	(goto-char (point-min))
+  	(setq lines (split-string (buffer-string) "\n" t))
+  	(setq random-line (nth (random (length lines)) lines))
+  	(setq random-line (string-join (split-string random-line) " ")))
+      (insert "    " random-line)))
+
+;; Function to insert ledger bal
+(defun pet/dashboard-ledger-monthly-balances (list-size) 
+  "Return the monthly balance by using ledger"
+  (interactive)
+  (dashboard-insert-heading "Monthly Balance:"
+  						      nil
+  						      (all-the-icons-faicon "money"
+  												:height 1.2
+  												:v-adjust 0.0
+  												:face 'dashboard-heading))
+  (insert "\n")
+  (let* ((current-month (format-time-string "%Y/%m"))
+  	       (cmd (format "ledger bal --flat --monthly --period %s %s "
+  					current-month
+  					(mapconcat 'identity pet/ledger-categories " "))))
+      (insert (shell-command-to-string cmd))))
+
 ;; Add Dashboard to Emacs
 (use-package dashboard
   :after (all-the-icons)
@@ -12,12 +48,24 @@
   (setq dashboard-startup-banner 'logo)
   ;; Set custom banner
   ;; (setq dashboard-startup-banner "~/.emacs.d/emacs-dash.png")
-  (setq dashboard-center-content nil) ;; set to 't' for centered content
+  (setq dashboard-center-content t) ;; set to 'nil' for uncentered content
+  (setq dashboard-show-shortcuts t)  ;; show shortcuts
+  (setq dashboard-projects-backend 'projectile)  ;; default ’projectile’
+  ;; (dashboard-agenda-sort-strategy '(priority-down))
   (setq dashboard-items '((recents . 5)
                           (agenda . 5 )
                           (bookmarks . 3)
                           (projects . 3)
-                          (registers . 3)))
+  						(registers . 3)
+  						;; (vocabulary)  ;; Needs vocabulary file
+  						(monthly-balance)))
+  (setq dashboard-item-generators '((monthly-balance . pet/dashboard-ledger-monthly-balances)
+  								      (vocabulary . pet/dashboard-insert-vocabulary)
+  								      (recents . dashboard-insert-recents)
+  								      (bookmarks . dashboard-insert-bookmarks)
+  								      (projects . dashboard-insert-projects)
+  								      (agenda . dashboard-insert-agenda)
+  								      (registers . dashboard-insert-registers)))
   :config
   (dashboard-setup-startup-hook)
   (dashboard-modify-heading-icons '((recents . "file-text")
