c2d7c58517531d48c1289ba14c84286aeb962133
RastiGiG <randomly.ventilates@simplelogin.co>
2024-01-23 06:15:02 +0100

    Emacs: Set magit-diff to display in side window

--- C01_EmacsConfiguration/.emacs.d/package_configs/magit_config.el
+++ C01_EmacsConfiguration/.emacs.d/package_configs/magit_config.el
@@ -1,6 +1,14 @@
 (use-package magit
   :bind ("C-x g" . magit-status)
   :commands (magit-status magit-get-current-branch)
+  :init
+  (add-to-list 'display-buffer-alist
+               '("magit-diff*"
+                 display-buffer-in-side-window
+                 (side . right)
+                 (slot . 3)
+                 (window-parameters . ((no-delete-other-windows . t)))
+                 (dedicated . t)))
   :custom
   (magit-display-buffer-function
     #'magit-display-buffer-same-window-except-diff-v1))
